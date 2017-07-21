7c9b8f3ea6daa55d87e9e41d2a4ae1dabea65e52
Tonee Young <itisi@toneeyoung.com>
2017-07-20 21:27:42 -0400

    Update auth, move options hashes around

--- watch_chat_waits.rb
+++ watch_chat_waits.rb
@@ -23,46 +23,40 @@
 require 'eventmachine'
 require 'em-http-request'
 require 'pp'
-require "/Users/user/Desktop/Pegacorn_Project/.gitignore/pegacorn_secrets"
+require '/Users/user/Desktop/Pegacorn_Project/.gitignore/pegacorn_secrets'
 
-EventMachine.run {
-	http = EventMachine::HttpRequest.new("wss://rtm.zopim.com/stream").get
-
+# May needto move OAuth token to its own hash, as well as the last three key/value
+# pairs, since they're the metric I want to subscribe to.  
+# The http item only takes 1-2 args tho, so I can't pass in three hash vars.
+EventMachine.run do
 	options = {
-          :connect_timeout => 5,        # default connection setup timeout
-          :inactivity_timeout => 10,    # default connection inactivity (post-setup) timeout
+        :connect_timeout => 5,        # default connection setup timeout
+        :inactivity_timeout => 10,    # default connection inactivity (post-setup) timeout
+        :Authorization => 'Bearer ' + ZendeskSecrets::OAUTH_SECRET,
+        :topic => "chats.waiting_time_avg",
+    	:action =>"subscribe",
+    	:window => 30
       }
 
- 	request_options = {
- 		:head => {
- 			'authorization' => [ZendeskSecrets::ZOPIM_USERNAME, ZendeskSecrets::ZOPIM_PASSWORD] 
- 		}
- 	}
+ 	http = EventMachine::HttpRequest.new('wss://rtm.zopim.com/stream', options).get
 
- 	# Not sure how to parse this subscription
- 	{
-    	topic: "chats.waiting_time_avg",
-    	action: "subscribe",
-    	window: 30
-	}
-
-	http.callback {
+	http.callback do
 		pp http.response_header.status
 		pp http.response_header
-	}
+	end
 
 	# Getting 0 for error code and nothing in the header, so it seems 
 	# like I'm not connecting at all
-	http.errback {
+	http.errback do
 		print "Uh oh, there was an error. \n"
 		pp http.response_header.status
 		pp http.response_header
-	}
+	end
  	
  		EventMachine.stop
- 	}
+ 	end
 
- 	puts "\n Ok, done."
+ 	puts "\nOk, done."
   
 # if waiting_time_avg <= 45
 # 	puts "Light the pegacorn!"
