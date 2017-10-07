185cbdafd68da2c3a25c58c01e87081d31e285f0
Tonee Young <itisi@toneeyoung.com>
2017-10-06 22:16:51 -0400

    Clean up conditional & force EventMachine to close after running

--- watch_chat_waits.rb
+++ watch_chat_waits.rb
@@ -2,7 +2,6 @@ require 'faye/websocket'
 require 'eventmachine'
 require '/Users/tyoung/Pegacorn_Project/.gitignore/pegacorn_secrets'
 require 'json'
-require 'awesome_print'
 
 EM.run do
   wss = Faye::WebSocket::Client.new('wss://rtm.zopim.com/stream',
@@ -15,33 +14,38 @@ EM.run do
     msg = {}
     msg[:topic] = 'chats.waiting_time_avg'
     msg[:action] = 'subscribe'
-    msg[:window] = 30
     wss.send msg.to_json
   end
 
+  # TODO : Set to try twice more if no results the first time.
+  # TODO : Trigger light if conditional is right.
+  # TODO : Set up cron job to trigger thisto run sometimes! 
   wss.on :message do |event|
     h = JSON.parse(event.data).to_hash
-    # ap [:message, h["content"]["data"]["waiting_time_avg"]]
-    puts 'Chat wait time average is currently'
-    if ![h.dig('content', 'data', 'waiting_time_avg')][0].to_i.zero? &&
-       [h.dig('content', 'data', 'waiting_time_avg')][0].to_i < 45
-    puts ": #{[h.dig('content', 'data', 'waiting_time_avg')][0]} seconds"
-      puts "\nLight the pegacorn!\======================================"
+    wait_time_avg = [h.dig('content', 'data', 'waiting_time_avg')][0]
+    print 'Chat wait time average is currently'
+    if !wait_time_avg.to_i.zero? &&
+       wait_time_avg.to_i < 45
+      puts " #{wait_time_avg} seconds"
+      puts "\nLight the pegacorn!\n"
     else
-    puts " not available."
-      puts "\nPegacorn time has not yet come.\n======================================"
+      puts ' not available.'
+      puts "\nPegacorn time has not yet come.\n"
     end
-    # if int > 5
-      # 'winner winner chicken dinner!'
-    # else
-      # 'not a winner'
-    # end
+	# pin = PiPiper::Pin.new( :pin => 17, :direction => :out )
+	# pin.off
+	# 1.times do
+	# 	pin.on
+	# 	sleep 15 #seconds
+	# 	pin.off
+	# end 
     wss = true
+    EM.stop
   end
 
   wss.on :close do |event|
+    puts "Something's gronked up." if event.code != 1006
     p [:close, event.code, event.reason]
-    # Make bell sound
-    # tput bel
+    EM.stop
   end
 end
