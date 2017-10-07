d5946a8677b173b6f856ab8b1938275f4a5aaeea
Tonee Young <itisi@toneeyoung.com>
2017-10-06 21:19:33 -0400

    Set up a conditional based on current chat wait time average

--- watch_chat_waits.rb
+++ watch_chat_waits.rb
@@ -2,7 +2,7 @@ require 'faye/websocket'
 require 'eventmachine'
 require '/Users/tyoung/Pegacorn_Project/.gitignore/pegacorn_secrets'
 require 'json'
-require 'PrettyPrint'
+require 'awesome_print'
 
 EM.run do
   wss = Faye::WebSocket::Client.new('wss://rtm.zopim.com/stream',
@@ -11,7 +11,7 @@ EM.run do
         { 'Authorization' => "Bearer #{ZendeskSecrets::ZENDESK_OAUTH_ACCESS_TOKEN}" })
 
   wss.on :open do
-    p [:open]
+    p [:start]
     msg = {}
     msg[:topic] = 'chats.waiting_time_avg'
     msg[:action] = 'subscribe'
@@ -20,13 +20,28 @@ EM.run do
   end
 
   wss.on :message do |event|
-    p [:message, event.data]
+    h = JSON.parse(event.data).to_hash
+    # ap [:message, h["content"]["data"]["waiting_time_avg"]]
+    puts 'Chat wait time average is currently'
+    if ![h.dig('content', 'data', 'waiting_time_avg')][0].to_i.zero? &&
+       [h.dig('content', 'data', 'waiting_time_avg')][0].to_i < 45
+    puts ": #{[h.dig('content', 'data', 'waiting_time_avg')][0]} seconds"
+      puts "\nLight the pegacorn!\======================================"
+    else
+    puts " not available."
+      puts "\nPegacorn time has not yet come.\n======================================"
+    end
+    # if int > 5
+      # 'winner winner chicken dinner!'
+    # else
+      # 'not a winner'
+    # end
     wss = true
   end
 
   wss.on :close do |event|
     p [:close, event.code, event.reason]
     # Make bell sound
-    tput bel
+    # tput bel
   end
 end
