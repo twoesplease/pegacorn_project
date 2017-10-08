12cb302ba0c7d6aff96540c111a42f8d0226a550
Tonee Young <itisi@toneeyoung.com>
2017-10-08 14:28:02 -0400

    Wait a bit and recheck the average wait time a couple more times if the result is nil/zero

--- watch_chat_waits.rb
+++ watch_chat_waits.rb
@@ -2,6 +2,7 @@ require 'faye/websocket'
 require 'eventmachine'
 require '/Users/tyoung/Pegacorn_Project/.gitignore/pegacorn_secrets'
 require 'json'
+require 'pi_piper'
 
 EM.run do
   wss = Faye::WebSocket::Client.new('wss://rtm.zopim.com/stream',
@@ -17,46 +18,31 @@ EM.run do
     wss.send msg.to_json
   end
 
-
-def pegacorn_trigger_check(event)
-  tries ||= 3
+  wss.on :message do |event|
   h = JSON.parse(event.data).to_hash
   wait_time_avg = [h.dig('content', 'data', 'waiting_time_avg')][0]
-  print 'Chat wait time average is currently'
-  if !wait_time_avg.to_i.zero? &&
-     wait_time_avg.to_i < 45
-    puts " #{wait_time_avg} seconds"
+  tries ||= 3
+  if wait_time_avg.to_i.zero? && !(tries -= 1).zero?
+    tries -= 1
+    sleep 15
+    redo
+  elsif !wait_time_avg.to_i.zero? &&
+    wait_time_avg.to_i < 45
+    puts "Chat wait time average is currently #{wait_time_avg} seconds"
     puts "\nLight the pegacorn!\n"
-  elsif wait_time_avg.to_i.zero?
-    puts ' in need of being re-checked.'
-    # recheck for a non-nil/non-zero wait time 2 more times
+    # pin = PiPiper::Pin.new( :pin => 17, :direction => :out )
+    # pin.off
+    # 1.times do
+    # 	pin.on
+    # 	sleep 15 #seconds
+    # 	pin.off
+    # end 
   else
-    puts ' not available.'
+    puts 'Chat wait time average is currently  not available.'
     puts "\nPegacorn time has not yet come.\n"
   end
-rescue
-  retry unless (tries -= 1).zero?
-end
-
-  # TODO : Set to try twice more if no results the first time.
-  # TODO : Break pegacorn_trigger_check into multiple methods so that
-  #        I can retry the first method if the wait time is zero.
-  #        Also want to retry the check a couple of times before
-  #        closing if the code isn't the successful 1006 code.
-  # TODO : Trigger light if conditional is right.
-  # TODO : Set up cron job to trigger this to run sometimes!
-
-  wss.on :message do |event|
-    pegacorn_trigger_check(event)
-	# pin = PiPiper::Pin.new( :pin => 17, :direction => :out )
-	# pin.off
-	# 1.times do
-	# 	pin.on
-	# 	sleep 15 #seconds
-	# 	pin.off
-	# end 
-    wss = true
-    EM.stop
+  wss = true
+  EM.stop
   end
 
   wss.on :close do |event|
